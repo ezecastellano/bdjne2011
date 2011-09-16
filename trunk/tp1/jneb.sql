44e1892fe063b510b0b6148c8c32ca7deded9cbe
ezequiel.castellano <ezequiel.castellano@59c153f0-7345-b197-527b-a8d1545af018>
2011-09-16 05:41:39 +0000

    Habían quedado dos columnas demás en el control... ahora lo modelabamos con la asignacion.

--- trunk/tp1/jneb.sql
+++ trunk/tp1/jneb.sql
@@ -21,10 +21,6 @@
 CREATE DATABASE IF NOT EXISTS test;
 USE test;
 
---
--- Definition of table `chofer`
---
-
 DROP TABLE IF EXISTS `chofer`;
 CREATE TABLE `chofer` (
   `numero_dni` int(8) unsigned NOT NULL,
@@ -37,19 +33,11 @@ CREATE TABLE `chofer` (
   UNIQUE KEY `chofer_licencia_numero` (`licencia_numero`) USING BTREE,
   CONSTRAINT `chofer_licencia_numero` FOREIGN KEY (`licencia_numero`) REFERENCES `licencia` (`numero`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `chofer`
---
 INSERT INTO `chofer` (`numero_dni`,`nombre`,`telefono`,`domicilio`,`fecha_nacimiento`,`licencia_numero`) VALUES 
  (19023489,'Juan Edi','NO TIENE','Av. Olazabal 2330','1940-01-22',2319),
  (22222222,'Mariano Semelman','2222-2222','Av. Lomas de Zamora 123','1989-08-25',34234),
  (33333333,'Carolina Hadad','5555-5555','Av. Barracas','1988-12-26',879519),
  (34496723,'Ezequiel Castellano','3641-5555','Av. Cabildo 262','1989-04-22',675689);
-
---
--- Definition of table `ciudad`
---
 
 DROP TABLE IF EXISTS `ciudad`;
 CREATE TABLE `ciudad` (
@@ -59,20 +47,12 @@ CREATE TABLE `ciudad` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `nombre_direccion` (`nombre`,`direccion`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ciudad`
---
 INSERT INTO `ciudad` (`id`,`nombre`,`direccion`) VALUES 
  (1,'Buenos Aires','Ciudad Universitaria,Pabellon I, Of. 234'),
  (3,'Cancún','Playa JNE'),
  (2,'Córdoba','Ciudad Universitaria,Pabellon IV, Of. 10'),
  (4,'La pedrera','Camping 2'),
  (5,'Ushuaia','Cabo del fin del mundo');
-
---
--- Definition of table `contingencia`
---
 
 DROP TABLE IF EXISTS `contingencia`;
 CREATE TABLE `contingencia` (
@@ -87,43 +67,20 @@ CREATE TABLE `contingencia` (
   CONSTRAINT `contingencia_viaje_fk` FOREIGN KEY (`viaje`) REFERENCES `viaje` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `contingencia`
---
-
---
--- Definition of table `control`
---
-
 DROP TABLE IF EXISTS `control`;
 CREATE TABLE `control` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `chofer` int(8) unsigned NOT NULL,
-  `viaje` int(10) unsigned NOT NULL,
   `tipo` int(10) unsigned NOT NULL,
   `resultado` varchar(45) NOT NULL,
   `fecha` date NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `contro_chofer_viaje` (`viaje`,`chofer`,`tipo`) USING BTREE,
-  KEY `control_chofer_numero_dni` (`chofer`) USING BTREE,
-  KEY `control_viaje_fk` (`viaje`),
   KEY `control_tipo_control_fk` (`tipo`),
-  CONSTRAINT `control_chofer_fk` FOREIGN KEY (`chofer`) REFERENCES `chofer` (`numero_dni`),
-  CONSTRAINT `control_tipo_control_fk` FOREIGN KEY (`tipo`) REFERENCES `tipo_control` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `control_viaje_fk` FOREIGN KEY (`viaje`) REFERENCES `viaje` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `control_tipo_control_fk` FOREIGN KEY (`tipo`) REFERENCES `tipo_control` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `control`
---
-INSERT INTO `control` (`id`,`chofer`,`viaje`,`tipo`,`resultado`,`fecha`) VALUES 
- (1,34496723,6,1,'NEGATIVO','2008-11-05'),
- (2,34496723,6,2,'POSITIVO','2011-10-10'),
- (3,33333333,4,3,'NEGATIVO','2011-01-01');
-
---
--- Definition of table `control_asignacion_chofer`
---
+INSERT INTO `control` (`id`,`tipo`,`resultado`,`fecha`) VALUES 
+ (1,1,'NEGATIVO','2008-11-05'),
+ (2,2,'POSITIVO','2011-10-10'),
+ (3,3,'NEGATIVO','2011-01-01');
 
 DROP TABLE IF EXISTS `control_asignacion_chofer`;
 CREATE TABLE `control_asignacion_chofer` (
@@ -137,14 +94,10 @@ CREATE TABLE `control_asignacion_chofer` (
   CONSTRAINT `control_asignacion_chofer_chofer_realizacion_viaje_fk` FOREIGN KEY (`chofer`, `realizacion_viaje`) REFERENCES `realizacion_viaje_chofer` (`chofer`, `realizacion_viaje`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `control_asignacion_chofer_control_fk` FOREIGN KEY (`control`) REFERENCES `control` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `control_asignacion_chofer`
---
-
---
--- Definition of table `licencia`
---
+INSERT INTO `control_asignacion_chofer` (`control`,`chofer`,`realizacion_viaje`) VALUES 
+ (3,33333333,4),
+ (1,34496723,6),
+ (2,34496723,6);
 
 DROP TABLE IF EXISTS `licencia`;
 CREATE TABLE `licencia` (
@@ -153,19 +106,11 @@ CREATE TABLE `licencia` (
   `fecha_renovacion` date NOT NULL,
   PRIMARY KEY (`numero`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `licencia`
---
 INSERT INTO `licencia` (`numero`,`fecha_obtencion`,`fecha_renovacion`) VALUES 
  (2319,'1970-03-13','2011-03-13'),
  (34234,'2008-01-13','2014-01-13'),
  (675689,'2001-11-10','2012-11-10'),
  (879519,'2004-01-13','2013-01-13');
-
---
--- Definition of table `observacion`
---
 
 DROP TABLE IF EXISTS `observacion`;
 CREATE TABLE `observacion` (
@@ -173,17 +118,9 @@ CREATE TABLE `observacion` (
   `descripcion` varchar(45) NOT NULL,
   PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `observacion`
---
 INSERT INTO `observacion` (`codigo`,`descripcion`) VALUES 
  (1,'USA ANTEOJOS'),
  (2,'MENOR DE EDAD');
-
---
--- Definition of table `observacion_licencia`
---
 
 DROP TABLE IF EXISTS `observacion_licencia`;
 CREATE TABLE `observacion_licencia` (
@@ -194,18 +131,10 @@ CREATE TABLE `observacion_licencia` (
   CONSTRAINT `observaciones_licencia_codigo` FOREIGN KEY (`observaciones_codigo`) REFERENCES `observacion` (`codigo`),
   CONSTRAINT `observaciones_licencia_numero` FOREIGN KEY (`licencia_numero`) REFERENCES `licencia` (`numero`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `observacion_licencia`
---
 INSERT INTO `observacion_licencia` (`observaciones_codigo`,`licencia_numero`) VALUES 
  (1,2319),
  (1,879519),
  (2,879519);
-
---
--- Definition of table `realizacion_viaje`
---
 
 DROP TABLE IF EXISTS `realizacion_viaje`;
 CREATE TABLE `realizacion_viaje` (
@@ -221,19 +150,11 @@ CREATE TABLE `realizacion_viaje` (
   CONSTRAINT `realizacion_viaje_vehiculo_fk` FOREIGN KEY (`vehiculo`) REFERENCES `vehiculo` (`patente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `realizacion_viaje_viaje_fk` FOREIGN KEY (`viaje`) REFERENCES `viaje` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `realizacion_viaje`
---
 INSERT INTO `realizacion_viaje` (`viaje`,`vehiculo`,`ruta`,`fecha_llegada`,`fecha_partida`) VALUES 
  (3,'CAS133',4,'2010-10-10','2010-10-09'),
  (6,'JNE011',5,'2011-11-10','2011-11-15'),
  (5,'JNE011',11,'2011-01-10','2011-01-15'),
  (4,'DLT098',3,'2011-01-10','2011-01-15');
-
---
--- Definition of table `realizacion_viaje_chofer`
---
 
 DROP TABLE IF EXISTS `realizacion_viaje_chofer`;
 CREATE TABLE `realizacion_viaje_chofer` (
@@ -245,19 +166,11 @@ CREATE TABLE `realizacion_viaje_chofer` (
   CONSTRAINT `realizacion_viaje_chofer_chofer_fk` FOREIGN KEY (`chofer`) REFERENCES `chofer` (`numero_dni`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `realizacion_viaje_chofer_realizacion_viaje_fk` FOREIGN KEY (`realizacion_viaje`) REFERENCES `realizacion_viaje` (`viaje`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `realizacion_viaje_chofer`
---
 INSERT INTO `realizacion_viaje_chofer` (`chofer`,`realizacion_viaje`) VALUES 
  (22222222,5),
  (33333333,4),
  (34496723,3),
  (34496723,6);
-
---
--- Definition of table `recorrido`
---
 
 DROP TABLE IF EXISTS `recorrido`;
 CREATE TABLE `recorrido` (
@@ -271,10 +184,6 @@ CREATE TABLE `recorrido` (
   CONSTRAINT `recorrido_ciudad_desde_fk` FOREIGN KEY (`ciudad_desde_id`) REFERENCES `ciudad` (`id`),
   CONSTRAINT `recorrido_ciudad_hasta_fk` FOREIGN KEY (`ciudad_hasta_id`) REFERENCES `ciudad` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `recorrido`
---
 INSERT INTO `recorrido` (`id`,`ciudad_hasta_id`,`ciudad_desde_id`) VALUES 
  (1,1,2),
  (2,1,3),
@@ -283,10 +192,6 @@ INSERT INTO `recorrido` (`id`,`ciudad_hasta_id`,`ciudad_desde_id`) VALUES
  (3,2,1),
  (7,2,4),
  (6,5,3);
-
---
--- Definition of table `ruta`
---
 
 DROP TABLE IF EXISTS `ruta`;
 CREATE TABLE `ruta` (
@@ -301,10 +206,6 @@ CREATE TABLE `ruta` (
   KEY `ruta_recorrido_fk` (`recorrido`),
   CONSTRAINT `ruta_recorrido_fk` FOREIGN KEY (`recorrido`) REFERENCES `recorrido` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ruta`
---
 INSERT INTO `ruta` (`id`,`cantidad_km`,`cantidad_peajes`,`condiciones_camino`,`condiciones_climaticas_periodo`,`tiempo_estimado`,`recorrido`) VALUES 
  (1,345.6,2,'Muy Buena.','','04:15:00',1),
  (2,120.6,2,'Mala.','','02:15:00',1),
@@ -318,28 +219,16 @@ INSERT INTO `ruta` (`id`,`cantidad_km`,`cantidad_peajes`,`condiciones_camino`,`c
  (10,102.6,2,'Buena.','','05:10:00',6),
  (11,102.6,2,'Buena.','','05:10:00',5);
 
---
--- Definition of table `tipo_contingencia`
---
-
 DROP TABLE IF EXISTS `tipo_contingencia`;
 CREATE TABLE `tipo_contingencia` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tipo_contingencia`
---
 INSERT INTO `tipo_contingencia` (`id`,`descripcion`) VALUES 
  (1,'ACCIDENTE'),
  (2,'RETRASO'),
  (3,'FALTA DE COMBUSTIBLE');
-
---
--- Definition of table `tipo_control`
---
 
 DROP TABLE IF EXISTS `tipo_control`;
 CREATE TABLE `tipo_control` (
@@ -347,18 +236,10 @@ CREATE TABLE `tipo_control` (
   `descripcion` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tipo_control`
---
 INSERT INTO `tipo_control` (`id`,`descripcion`) VALUES 
  (1,'ALCHOLEMIA'),
  (2,'ESTADO FISICO'),
  (3,'DROGAS');
-
---
--- Definition of table `vehiculo`
---
 
 DROP TABLE IF EXISTS `vehiculo`;
 CREATE TABLE `vehiculo` (
@@ -370,20 +251,12 @@ CREATE TABLE `vehiculo` (
   `fecha_ingreso_taller` date DEFAULT NULL,
   PRIMARY KEY (`patente`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `vehiculo`
---
 INSERT INTO `vehiculo` (`patente`,`capacidad`,`modelo`,`marca`,`fecha_compra`,`fecha_ingreso_taller`) VALUES 
  ('BJH089',5,'FOCUS','FORD','2006-03-01','2011-08-15'),
  ('BLA123',5,'19','RENAULT','2009-04-01',NULL),
  ('CAS133',20,'Trafic','RENAULT','2011-01-01',NULL),
  ('DLT098',5,'GOL DUBLIN 3P','VOLSKWAGEN','2008-08-01',NULL),
  ('JNE011',8,'806','VOLSKWAGEN','2010-01-01',NULL);
-
---
--- Definition of table `viaje`
---
 
 DROP TABLE IF EXISTS `viaje`;
 CREATE TABLE `viaje` (
@@ -399,10 +272,6 @@ CREATE TABLE `viaje` (
   CONSTRAINT `viaje_recorrido_fk` FOREIGN KEY (`recorrido`) REFERENCES `recorrido` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `viaje_vehiculo_fk` FOREIGN KEY (`vehiculo`) REFERENCES `vehiculo` (`patente`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `viaje`
---
 INSERT INTO `viaje` (`id`,`recorrido`,`vehiculo`,`fecha_partida_planificada`,`fecha_llegada_planificada`) VALUES 
  (1,1,'DLT098','2011-10-09','2011-10-12'),
  (2,1,'BLA123','2012-11-01','2012-11-02'),
@@ -410,10 +279,6 @@ INSERT INTO `viaje` (`id`,`recorrido`,`vehiculo`,`fecha_partida_planificada`,`fe
  (4,3,'DLT098','2010-11-01','2011-07-02'),
  (5,5,'JNE011','2011-09-01','2011-09-02'),
  (6,2,'JNE011','2011-09-01','2011-09-02');
-
---
--- Definition of table `viaje_chofer`
---
 
 DROP TABLE IF EXISTS `viaje_chofer`;
 CREATE TABLE `viaje_chofer` (
@@ -425,10 +290,6 @@ CREATE TABLE `viaje_chofer` (
   CONSTRAINT `viaje_chofer_chofer_fk` FOREIGN KEY (`chofer`) REFERENCES `chofer` (`numero_dni`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `viaje_chofer_viaje_fk` FOREIGN KEY (`viaje`) REFERENCES `viaje` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `viaje_chofer`
---
 INSERT INTO `viaje_chofer` (`viaje`,`chofer`) VALUES 
  (1,19023489),
  (2,19023489),
@@ -439,10 +300,6 @@ INSERT INTO `viaje_chofer` (`viaje`,`chofer`) VALUES
  (5,19023489),
  (5,34496723),
  (6,22222222);
-
---
--- Definition of procedure `choferesQueManejaronTodosLosNuevos`
---
 
 DROP PROCEDURE IF EXISTS `choferesQueManejaronTodosLosNuevos`;
 
@@ -467,10 +324,6 @@ END $$
 
 DELIMITER ;
 
---
--- Definition of procedure `recorridosConTodasRutasUsadas`
---
-
 DROP PROCEDURE IF EXISTS `recorridosConTodasRutasUsadas`;
 
 DELIMITER $$
@@ -492,10 +345,6 @@ END $$
 /*!50003 SET SESSION SQL_MODE=@TEMP_SQL_MODE */  $$
 
 DELIMITER ;
-
---
--- Definition of procedure `viajesRealizadosYEstadoPorVehiculo`
---
 
 DROP PROCEDURE IF EXISTS `viajesRealizadosYEstadoPorVehiculo`;
 
