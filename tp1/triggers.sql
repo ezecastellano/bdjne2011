6fdffd3e6581ce27d19829db879c841c27cb4e05
ezequiel.castellano <ezequiel.castellano@59c153f0-7345-b197-527b-a8d1545af018>
2011-09-16 14:32:19 +0000

    Dos trigers sobre la asignación de vehiculos en uso.

---
+++ tp1/triggers.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+CREATE TRIGGER evitarPlanificarConCochesEnReparacion BEFORE INSERT ON viaje
+  FOR EACH ROW BEGIN
+    DECLARE enUso BOOLEAN;
+    SELECT (fecha_ingreso_taller is null) INTO enUso FROM vehiculo v WHERE v.patente=NEW.vehiculo;
+    IF NOT enUso THEN
+      SET NEw.vehiculo=null;
+    END IF;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER evitarRealizarViajesConCochesEnReparacion BEFORE INSERT ON realizacion_viaje
+  FOR EACH ROW BEGIN
+    DECLARE enUso BOOLEAN;
+    SELECT (fecha_ingreso_taller is null) INTO enUso FROM vehiculo v WHERE v.patente=NEW.vehiculo;
+    IF NOT enUso THEN
+      SET NEw.vehiculo=null;
+    END IF;
+END $$
+DELIMITER ;
